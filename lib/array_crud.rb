747c2f27d1045b73745633686a5350721f1eb800
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-11 05:01:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,11 +11,11 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop(element)
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
